ffe9b0ba0cb3429edb16db817ac8acf8d5662de8
MyNextWeekend <jinhu007@outlook.com>
2025-10-16 22:27:02 +0800

    🐞 fix: 修改导入方式

--- src/core/log.rs
+++ src/core/log.rs
@@ -2,29 +2,32 @@ use std::sync::Arc;
 
 use tracing::info;
 use tracing_appender::{non_blocking::WorkerGuard, rolling};
-use tracing_subscriber::{EnvFilter, Layer, fmt, layer::SubscriberExt, util::SubscriberInitExt};
+use tracing_subscriber::{Layer, layer::SubscriberExt, util::SubscriberInitExt};
 
 use crate::core::config::AppConfig;
 
 pub fn init(config: Arc<AppConfig>) -> WorkerGuard {
-    let file_filter = EnvFilter::new(&config.log.file_level);
-    let console_filter = EnvFilter::new(&config.log.console_level);
+    let file_filter = tracing_subscriber::EnvFilter::new(&config.log.file_level);
+    let console_filter = tracing_subscriber::EnvFilter::new(&config.log.console_level);
 
     // --- 文件日志 ---
     let file_appender = rolling::daily(&config.log.directory, &config.log.file_name);
     let (non_blocking, guard) = tracing_appender::non_blocking(file_appender);
 
     // --- 文件日志 Layer ---
-    let file_layer = fmt::layer()
+    let file_layer = tracing_subscriber::fmt::layer()
         .with_writer(non_blocking)
         .with_ansi(false) // 文件禁用彩色
         .with_target(true)
         .with_filter(file_filter);
 
     // --- 控制台 Layer ---
-    let console_layer = fmt::layer()
+    let console_layer = tracing_subscriber::fmt::layer()
         .with_ansi(false) // 控制台等级彩色
-        .with_target(true)
+        .with_file(true)
+        .with_line_number(true)
+        .with_thread_ids(true)
+        .with_thread_names(true)
         .with_filter(console_filter);
 
     // --- 组合日志 ---
